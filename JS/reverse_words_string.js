b79f634e3e11d2f714c46b97481a7353c1a65857
Mikhail8425 <mikhailshemet@gmail.com>
2024-03-11 21:49:19 -0400

    reverseWords str

---
+++ JS/reverse_words_string.js
@@ -0,0 +1,9 @@
+function reverseWords(str) {
+  // Go for it
+  return str.split(' ').map(word => word.split('').reverse().join('')).join(' ');
+}
+
+console.log(reverseWords('The quick brown fox jumps over the lazy dog.'));
+console.log(reverseWords('apple'));
+console.log(reverseWords('a b c d'));
+console.log(reverseWords('double  spaced  words'));
