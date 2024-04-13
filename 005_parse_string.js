19d00456f7585d27b982eca25842c3b43dcce9de
Mikhail8425 <mikhailshemet@gmail.com>
2024-04-12 21:06:17 -0400

    adding function

---
+++ 005_parse_string.js
@@ -0,0 +1,10 @@
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = "cnet"
+
+function domainName(url){
+  url = url.replace()
+}
+
+const url = "https://www.cnet.com"
+console.log(domainName(url))
